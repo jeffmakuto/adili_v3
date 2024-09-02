f4d380e1df0e63cc3af4e5af82ca532e6cecb041
jeffmakuto <hjeffmakuto@gmail.com>
2024-09-02 17:58:27 +0300

    chore: remove semi colons on imports and use single quotation marks

--- adili/src/app/layout.tsx
+++ adili/src/app/layout.tsx
@@ -1,7 +1,7 @@
-import type { Metadata } from "next";
-import { Roboto_Flex } from "next/font/google";
-import "./globals.css";
-import { NavLinks } from "@/app/ui/nav-links";
+import type { Metadata } from 'next'
+import { Roboto_Flex } from 'next/font/google'
+import './globals.css'
+import { NavLinks } from '@/app/ui/nav-links'
 
 const robotoFlex = Roboto_Flex({ subsets: ["latin"] });
 
