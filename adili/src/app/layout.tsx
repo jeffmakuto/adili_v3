93b8d62d9eedfe46b23a3dea3b0599f14a1d7f7d
jeffmakuto <hjeffmakuto@gmail.com>
2024-09-02 17:53:33 +0300

    feat: import the navigation links to the root layout

--- adili/src/app/layout.tsx
+++ adili/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Roboto_Flex } from "next/font/google";
 import "./globals.css";
+import { NavLinks } from "@/app/ui/nav-links";
 
 const robotoFlex = Roboto_Flex({ subsets: ["latin"] });
 
@@ -8,15 +9,14 @@ export const metadata: Metadata = {
   title: "Kenya Airways | Ethics & Integrity",
   description: "Explore Kenya Airways' commitment to ethics and integrity. Discover our mission, vision, values, Integrity Code and other policies.",
 };
-
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+ 
+export default function Layout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang="en">
-      <body className={robotoFlex.className}>{children}</body>
+    <html lang="en" className={robotoFlex.className}>
+      <body>
+        <NavLinks />
+        <main>{children}</main>
+      </body>
     </html>
-  );
+  )
 }
