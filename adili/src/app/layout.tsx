9f2bebbaa6474f792a2bde08cb3799481f1aa86e
jeffmakuto <hjeffmakuto@gmail.com>
2024-09-03 10:37:48 +0300

    refactor: update Layout to use GlobalError component in ErrorBoundary

--- adili/src/app/layout.tsx
+++ adili/src/app/layout.tsx
@@ -2,20 +2,24 @@ import type { Metadata } from 'next'
 import { Roboto_Flex } from 'next/font/google'
 import './globals.css'
 import { NavLinks } from '@/app/ui/nav-links'
+import GlobalError from '@/app/global-error'
+import { ErrorBoundary } from 'react-error-boundary'
 
 const robotoFlex = Roboto_Flex({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
   title: "Kenya Airways | Ethics & Integrity",
-  description: "Explore Kenya Airways' commitment to ethics and integrity. Discover our mission, vision, values, Integrity Code and other policies.",
+  description: "Explore Kenya Airways' commitment to ethics and integrity.",
 };
- 
+
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en" className={robotoFlex.className}>
       <body>
         <NavLinks />
-        <main>{children}</main>
+        <ErrorBoundary FallbackComponent={GlobalError}>
+          <main>{children}</main>
+        </ErrorBoundary>
       </body>
     </html>
   )
