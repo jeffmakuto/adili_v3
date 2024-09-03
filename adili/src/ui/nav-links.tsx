2982d1a3ac75a02426ef3ed982911782b98fc5d6
jeffmakuto <hjeffmakuto@gmail.com>
2024-09-03 16:02:07 +0300

    refactor: simplify NavLinks component by using a link array

--- adili/src/ui/nav-links.tsx
+++ adili/src/ui/nav-links.tsx
@@ -3,42 +3,28 @@
 import { usePathname } from 'next/navigation'
 import Link from 'next/link'
 
+const links = [
+  { href: '/', label: 'Home' },
+  { href: '/policies', label: 'Policies' },
+  { href: '/case-studies', label: 'Case Studies' },
+  { href: '/act-right-speak-up', label: 'Act Right Speak Up' },
+  { href: '/adili-chat', label: 'Adili Chat' }
+];
+
 export function NavLinks() {
-  const pathname = usePathname()
+  const pathname = usePathname();
 
   return (
-    <nav>
-      <Link className={`link ${pathname === '/' ? 'active' : ''}`} href="/">
-        Home
-      </Link>
-
-      <Link
-        className={`link ${pathname === '/policies' ? 'active' : ''}`}
-        href="/policies"
-      >
-        Policies
-      </Link>
-
-      <Link
-        className={`link ${pathname === '/case-studies' ? 'active' : ''}`}
-        href="/case-studies"
-      >
-        Case Studies
-      </Link>
-
-      <Link
-        className={`link ${pathname === '/act-right-speak-up' ? 'active' : ''}`}
-        href="/act-right-speak-up"
-      >
-        Act Right Speak Up
-      </Link>
-
-      <Link
-        className={`link ${pathname === '/adili-chat' ? 'active' : ''}`}
-        href="/adili-chat"
-      >
-        Adili Chat
-      </Link>
+    <nav aria-label="Main site navigation">
+      {links.map((link) => (
+        <Link
+          key={link.href}
+          className={`link ${pathname === link.href ? 'active' : ''}`}
+          href={link.href}
+        >
+          {link.label}
+        </Link>
+      ))}
     </nav>
-  )
+  );
 }
