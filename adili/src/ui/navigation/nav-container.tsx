cb0541ed9184e62435f72b4aa4109894a43813c1
jeffmakuto <hjeffmakuto@gmail.com>
2024-09-04 16:39:57 +0300

    chore: update navlink container to cover full viewport width

--- adili/src/ui/navigation/nav-container.tsx
+++ adili/src/ui/navigation/nav-container.tsx
@@ -2,7 +2,7 @@ import NavList from '@/ui/navigation/nav-list'
 import { NavProps } from '@/types/link'
 
 const NavContainer: React.FC<NavProps> = ({ links }) => (
-  <div className="p-4 bg-gray-100 w-[90%] mx-auto box-border">
+  <div className="p-4 bg-gray-100 mx-auto box-border">
     <nav aria-label="Main site navigation">
       <div className="overflow-x-auto">
         <NavList links={links} />
