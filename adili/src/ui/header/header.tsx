35e3947674f805ba5421b33a9a4b8e06dc5fe3e8
jeffmakuto <hjeffmakuto@gmail.com>
2024-09-05 18:07:04 +0300

    refactor: adjust Header component layout
    - Changed the Header component to use a vertical layout with flex-col.
    - Adjusted the positioning of Banner and NavLinks using responsive flexbox utilities.
    - Updated Banner and NavLinks to center-align on smaller screens and align to the right or left on larger screens.

--- adili/src/ui/header/header.tsx
+++ adili/src/ui/header/header.tsx
@@ -3,14 +3,12 @@ import NavLinks from '@/ui/navigation/nav-links'
 
 export default function Header() {
     return (
-        <header>
-            <div className="w-full flex justify-between items-center px-4 py-2">
-                <div className="w-2/5 flex">
-                    <NavLinks />
-                </div>
-                <div className="w-3/10 text-right">
-                    <Banner />
-                </div>
+        <header className="flex flex-col">
+            <div className="w-full flex justify-center md:justify-end lg:justify-end">
+                <Banner />
+            </div>
+            <div className="w-full flex justify-center md:justify-start lg:justify-start">
+                <NavLinks />
             </div>
         </header>
     );
