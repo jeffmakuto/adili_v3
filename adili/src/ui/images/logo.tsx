d0dbe20628712d5c81f795ff0c042451886ee8e7
jeffmakuto <hjeffmakuto@gmail.com>
2024-09-06 11:24:09 +0300

    feat: update the logo image and adjust the size

--- adili/src/ui/images/logo.tsx
+++ adili/src/ui/images/logo.tsx
@@ -2,9 +2,9 @@ import Image from 'next/image';
 
 const Logo = () => {
   return (
-    <div className="relative w-5/12 h-16 md:w-full md:h-24 lg:w-7/12 lg:h-32">
+    <div className="relative w-24 h-16 md:w-60 md:h-52">
       <Image
-        src="/images/KQ Logo.jpg"
+        src="/images/logo.png"
         alt="Company Logo"
         fill
         sizes="(max-width: 768px) 12rem, (max-width: 1024px) 16rem, 20rem"
