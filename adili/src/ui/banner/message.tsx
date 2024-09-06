ee04c197127b17ad0ab9eb861ae50229d244d369
jeffmakuto <hjeffmakuto@gmail.com>
2024-09-06 15:57:50 +0300

    style: auto adjust the banner message for responsiveness in different screen sizes

--- adili/src/ui/banner/message.tsx
+++ adili/src/ui/banner/message.tsx
@@ -1,8 +1,12 @@
 export default function BannerMessage() {
   return (
-    <div className="text-center">
-      <h3 className="text-xl font-bold mb-2 text-red-600">Adili Chat</h3>
-      <p className="mb-4 text-lg">Get quick answers, resources, and support</p>
+    <div className="text-center md:text-left">
+      <h3 className="text-base sm:text-lg md:text-xl lg:text-2xl font-bold mb-2 text-red-600">
+        Adili Chat
+      </h3>
+      <p className="text-sm sm:text-base md:text-lg lg:text-xl mb-4">
+        Get quick answers, resources, and support
+      </p>
     </div>
   );
 }
