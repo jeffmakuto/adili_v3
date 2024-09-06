75f1f931fa00535b36ea15c1563340cbda33aa04
jeffmakuto <hjeffmakuto@gmail.com>
2024-09-06 08:36:50 +0300

    style: update the header component - make the header fixed at the top of the viewport (top-0) and span the full width of the screen (w-full) - z-50: ensure the header stays above all other content - shadow-lg: Add a shadow to create a distinct separation between the header and the content below it -bg-white: Since it's fixed, we make sure the background is white to prevent any transparency over the page content

--- adili/src/ui/header/header.tsx
+++ adili/src/ui/header/header.tsx
@@ -4,9 +4,9 @@ import BannerImage from '@/ui/images/banner'
 
 export default function Header() {
     return (
-        <header className="flex flex-col p-4">
+        <header className="fixed top-0 w-full flex flex-col p-4 z-50">
             {/* Logo positioned on the extreme left for medium and large screens */}
-            <div className="w-full flex justify-center md:justify-between lg:justify-between">
+            <div className="w-full flex justify-center md:justify-between lg:justify-between items-center">
                 {/* Hide on small screens and show on medium and larger */}
                 <div className="hidden md:block lg:block">
                     <BannerImage imageSrc='/images/KQ Logo.jpg' />
