5788f78aa4f448e5e74b98f8e44e1bc9855e0e0a
jeffmakuto <hjeffmakuto@gmail.com>
2024-09-06 11:43:48 +0300

    chore: update logo alt

--- adili/src/ui/images/logo.tsx
+++ adili/src/ui/images/logo.tsx
@@ -5,7 +5,7 @@ const Logo = () => {
     <div className="relative w-48 h-40 lg:w-60 lg:h-40">
       <Image
         src="/images/logo.png"
-        alt="Company Logo"
+        alt="Ethics & Integrity Logo"
         fill
         sizes="(max-width: 768px) 12rem, (max-width: 1024px) 16rem, 20rem"
         priority
