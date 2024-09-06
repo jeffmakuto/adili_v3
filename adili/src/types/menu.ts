ad83ff482064f044a72778f0b6c999d361df4ff3
jeffmakuto <hjeffmakuto@gmail.com>
2024-09-06 15:58:57 +0300

    feat: create additional types for the mobile  menu

--- adili/src/types/menu.ts
+++ adili/src/types/menu.ts
@@ -14,4 +14,13 @@ export type MobileMenuProps = {
 export type MenuItemProps = LinkType & {
     onClick?: () => void;
 };
-  
+
+export type MenuContentProps = {
+    toggleMenu: () => void;
+    links: LinkType[];
+    menuOpen: boolean;
+};
+
+export type OverlayProps = {
+    onClick: () => void;
+};
