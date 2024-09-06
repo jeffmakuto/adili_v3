0310f6f41da4f019c88be77f1ab96c01affbc15b
jeffmakuto <hjeffmakuto@gmail.com>
2024-09-06 10:02:36 +0300

    feat: update the mobile menu button

--- adili/src/ui/menu/hamburger.tsx
+++ adili/src/ui/menu/hamburger.tsx
@@ -4,12 +4,12 @@ const HamburgerMenuButton: React.FC<HamburgerMenuButtonProps> = ({ menuOpen, tog
   return (
     <button
       onClick={toggleMenu}
-      className="flex items-center text-red-500 focus:outline-none"
+      className="flex items-center text-red-500 focus:outline-none p-2 lg:p-4"
       aria-label={menuOpen ? 'Close menu' : 'Open menu'}
       aria-expanded={menuOpen}
     >
       <svg
-        className="w-8 h-8 mr-2"
+        className="w-6 h-6 lg:w-8 lg:h-8 mr-2"
         fill="none"
         stroke="red"
         viewBox="0 0 24 24"
@@ -18,11 +18,11 @@ const HamburgerMenuButton: React.FC<HamburgerMenuButtonProps> = ({ menuOpen, tog
         <path
           strokeLinecap="round"
           strokeLinejoin="round"
-          strokeWidth="5"
+          strokeWidth="2"
           d="M4 6h16M4 12h16M4 18h16"
         />
       </svg>
-      <span className="text-red-500 font-black">MENU</span>
+      <span className="text-red-500 font-bold lg:text-lg">MENU</span>
     </button>
   );
 };
