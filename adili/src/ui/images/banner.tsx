10a14c926d9ddf6c62f8c7d32f21cd6d1f9e9566
jeffmakuto <hjeffmakuto@gmail.com>
2024-09-06 12:49:46 +0300

    style: adjust banner image to center it in the circle

--- adili/src/ui/images/banner.tsx
+++ adili/src/ui/images/banner.tsx
@@ -3,14 +3,15 @@ import { BannerImageProps } from '@/types/banners'
 
 export default function BannerImage({ imageSrc }: BannerImageProps) {
   return (
-    <div className="relative w-44 h-48 md:w-56 md:h-60">
+    <div className="relative w-44 h-48 md:w-56 md:h-60 lg:w-72 lg:h-80">
       <Image
         src={imageSrc}
         alt="Adili Chat advert pop up notification"
         fill
-        sizes="(max-width: 640px) 50vw, 25vw"
+        sizes="(max-width: 640px) 50vw, (max-width: 768px) 25vw, 20vw"
         style={{
           objectFit: 'cover',
+          objectPosition: 'center 20%',
           borderRadius: '9999px',
           border: '4px solid white',
           boxShadow: '0 4px 6px rgba(0, 0, 0, 0.1)',
