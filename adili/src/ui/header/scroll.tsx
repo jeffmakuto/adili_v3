ccb1b180a40650a9065cf129dbd7c83a8655216e
jeffmakuto <hjeffmakuto@gmail.com>
2024-09-09 09:39:15 +0300

    feat: create a json file for the scrolling message - Update the scroll.tsx file to use it

--- adili/src/ui/header/scroll.tsx
+++ adili/src/ui/header/scroll.tsx
@@ -1,6 +1,7 @@
 'use client'
 
 import useScrollingAnimation from '@/hooks/scroll'
+import scrollContent from '@/data/scroll-content.json'
 
 const ScrollingMessage: React.FC = () => {
   const messageRef = useScrollingAnimation();
@@ -8,20 +9,23 @@ const ScrollingMessage: React.FC = () => {
   return (
     <div className="w-full py-2 shadow-lg">
       <div className="flex items-center whitespace-nowrap overflow-hidden">
-        <div 
-          className="animate-scroll"
-          ref={messageRef}
-        >
+        <div className="animate-scroll" ref={messageRef}>
           <span className="text-red-600 px-4 text-2xl">&#9733;</span>
           <span className="px-4 text-sm md:text-base lg:text-lg">
-            <strong className="text-red-600 text-base md:text-lg lg:text-xl">Our Mission:</strong>
-            <span className="text-black"> To propel Africa&apos;s prosperity by connecting its people, cultures, and markets.</span>
+            <strong className="text-red-600 text-base md:text-lg lg:text-xl">
+              {scrollContent.mission.title}
+            </strong>
+            <span className="text-black"> {scrollContent.mission.description}</span>
             <span className="px-2 text-red-600 text-xl" style={{ fontWeight: 'bold' }}>|</span>
-            <strong className="text-red-600 text-base md:text-lg lg:text-xl">Our Vision:</strong>
-            <span className="text-black"> To be Africa&apos;s preferred and sustainable aviation group.</span>
+            <strong className="text-red-600 text-base md:text-lg lg:text-xl">
+              {scrollContent.vision.title}
+            </strong>
+            <span className="text-black"> {scrollContent.vision.description}</span>
             <span className="px-2 text-red-600 text-xl" style={{ fontWeight: 'bold' }}>|</span>
-            <strong className="text-red-600 text-base md:text-lg lg:text-xl">Our Values:</strong>
-            <span className="text-black"> Safety, Customer Obsession, Integrity, Accountability</span>
+            <strong className="text-red-600 text-base md:text-lg lg:text-xl">
+              {scrollContent.values.title}
+            </strong>
+            <span className="text-black"> {scrollContent.values.description}</span>
           </span>
           <span className="text-red-600 px-4 text-2xl">&#9733;</span>
         </div>
