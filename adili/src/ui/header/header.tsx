74e5f5f96e04cafdf260f1bc63c795565c913ad6
jeffmakuto <hjeffmakuto@gmail.com>
2024-09-09 13:56:31 +0300

    styel: set a background color to prevent any overlap issues with transparent backgrounds.

--- adili/src/ui/header/header.tsx
+++ adili/src/ui/header/header.tsx
@@ -4,7 +4,7 @@ import Nav from '@/ui/navigation/nav';
 export default function Header() {
   return (
     <header>
-      <div className="fixed top-0 w-full flex flex-col z-50">
+      <div className="fixed top-0 w-full flex flex-col z-50 bg-white">
         <ScrollingMessage />
       </div>
       <div className="mt-16">
