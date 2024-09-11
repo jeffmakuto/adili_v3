653aeb56fdcf34d229937c7c2740919732dca451
jeffmakuto <hjeffmakuto@gmail.com>
2024-09-11 14:06:44 +0300

    feat: create policies, case studies, our ambassadors, and adili chat page.tsx

---
+++ adili/src/app/adili-chat/page.tsx
@@ -0,0 +1,8 @@
+
+export default function AdiliChat() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Ethics & Integrity</h1>
+    </main>
+  );
+}
---
+++ adili/src/app/case-studies/page.tsx
@@ -0,0 +1,8 @@
+
+export default function CaseStudies() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <h1>Ethics & Integrity</h1>
+    </main>
+  );
+}
