798a3c901e3e4b90897a1611296b967cb7e48714
jeffmakuto <hjeffmakuto@gmail.com>
2024-09-16 15:59:52 +0300

    feat: add the redline between the hero section and values section

--- adili/src/app/page.tsx
+++ adili/src/app/page.tsx
@@ -1,10 +1,12 @@
 import Hero from '@/ui/home/hero'
+import RedLine from '@/ui/home/red-line'
 import Values from '@/ui/home/values'
 
 const Home: React.FC = () => {
   return (
     <main className="mt-8">
       <Hero />
+      <RedLine />
       <Values />
     </main>
   );
---
+++ adili/src/ui/home/red-line.tsx
@@ -0,0 +1,10 @@
+const RedLine: React.FC = () => {
+  return (
+    <div>
+      {/* Static Red Line */}
+      <div className="w-full h-2 bg-red-500"></div>
+    </div>
+  );
+};
+
+export default RedLine;
