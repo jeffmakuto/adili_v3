75cb174eb2b5cea6842efd063ed5e19ee1d2c6ab
jeffmakuto <hjeffmakuto@gmail.com>
2024-09-17 17:10:28 +0300

    refactor: centralize footer styling in one file
    - create a footer styling file
    - update footer links horizontal spacing

---
+++ adili/src/styles/footer.ts
@@ -0,0 +1,9 @@
+export const footerStyles = {
+    container: "w-full p-8 bg-[#0d0d0d] relative",
+    content: "max-w-5xl px-4 mx-auto flex flex-col items-center",
+    copyright: "text-white text-center text-sm md:text-sm lg:text-lg mt-8",
+    linksContainer: "flex flex-col md:flex-row md:space-x-16 mb-4",
+    link: "text-white hover:underline mb-2 md:mb-0 text-sm md:text-sm lg:text-lg",
+    separator: "text-white",
+  };
+  
--- adili/src/ui/footer/footer.tsx
+++ adili/src/ui/footer/footer.tsx
@@ -1,18 +1,21 @@
 import ReturnToTopButton from '@/ui/buttons/return-to-top'
+import { footerStyles } from '@/styles/footer'
 
 const Footer: React.FC = () => {
   return (
-    <footer className="w-full py-6 bg-[#0d0d0d] text-white relative">
-      <div className="max-w-5xl px-4 mx-auto flex flex-col items-center">
-        <p className="text-center text-xs sm:text-sm mb-2">
-          &copy; {new Date().getFullYear()} KQ Ethics & Integrity. All rights reserved.
-        </p>
-        <div className="flex flex-col md:flex-row md:space-x-6 mb-4">
-          <a href="#" className="text-red-600 hover:underline mb-2 md:mb-0 text-xs sm:text-sm">Privacy Policy</a>
-          <a href="#" className="text-red-600 hover:underline mb-2 md:mb-0 text-xs sm:text-sm">Terms of Service</a>
-          <a href="#" className="text-red-600 hover:underline text-xs sm:text-sm">Cookie Policy</a>
+    <footer className={footerStyles.container}>
+      <div className={footerStyles.content}>
+        <div className={footerStyles.linksContainer}>
+          <a href="#" className={footerStyles.link}>Privacy Policy</a>
+          <span className={footerStyles.separator}>|</span>
+          <a href="#" className={footerStyles.link}>Terms of Service</a>
+          <span className={footerStyles.separator}>|</span>
+          <a href="#" className={footerStyles.link}>Cookie Policy</a>
         </div>
         <ReturnToTopButton />
+        <p className={footerStyles.copyright}>
+          &copy; {new Date().getFullYear()} KQ Ethics & Integrity. All rights reserved.
+        </p>
       </div>
     </footer>
   );
