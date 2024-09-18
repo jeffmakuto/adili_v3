9e47a26b35d0074283b45cf22af0b48df508e339
jeffmakuto <hjeffmakuto@gmail.com>
2024-09-18 15:45:57 +0300

    feat: adjust the scrolling message code to remove redundnacy in parts of the code
    - update scroll duration

--- adili/src/ui/header/scroll.tsx
+++ adili/src/ui/header/scroll.tsx
@@ -1,32 +1,33 @@
 'use client'
-
+import React from 'react'
 import useScrollingAnimation from '@/hooks/scroll'
 import scrollContent from '@/data/scroll-content.json'
 
 const ScrollingMessage: React.FC = () => {
-  const scrollDuration = 30;
+  const scrollDuration = 60;
   const messageRef = useScrollingAnimation(scrollDuration);
+
+  const { title, description } = scrollContent.contents[0];
+  const items = description.split(' • ');
 
   return (
     <div className="w-full py-2 shadow-lg">
       <div className="flex items-center whitespace-nowrap overflow-hidden">
         <div className="animate-scroll" ref={messageRef}>
           <span className="text-red-600 px-4 text-2xl">&#9733;</span>
-
-          {scrollContent.contents.map((content, index) => (
-            <span key={index} className="px-4 text-sm md:text-base lg:text-lg">
-              <strong className="text-red-600 text-base md:text-lg lg:text-xl">
-                {content.title}
-              </strong>
-              <span className="text-black"> {content.description}</span>
-              
-              {/* Add a separator unless it's the last item */}
-              {index < scrollContent.contents.length - 1 && (
-                <span className="px-2 text-red-600 text-xl" style={{ fontWeight: 'bold' }}>|</span>
-              )}
+          <span className="px-4 text-sm md:text-base lg:text-lg">
+            <strong className="text-red-600 text-base md:text-lg lg:text-xl">
+              {title}
+            </strong>
+            <span className="text-black">
+              {items.map((item, index) => (
+                <React.Fragment key={index}>
+                  {index > 0 && <span className="text-red-600 px-2 text-xl font-bold">|</span>}
+                  {item}
+                </React.Fragment>
+              ))}
             </span>
-          ))}
-
+          </span>
           <span className="text-red-600 px-4 text-2xl">&#9733;</span>
         </div>
       </div>
