ee6cb7e7c8319465bbf6c000a64321cbb73cac56
jeffmakuto <hjeffmakuto@gmail.com>
2024-09-18 09:29:09 +0300

    feat: adjust slider images width and height for a quality display

--- adili/src/data/slider.ts
+++ adili/src/data/slider.ts
@@ -1,11 +1,10 @@
 import { ImageData } from '@/types/slider'
 
 export const images: ImageData[] = [
-    { src: '/images/mission.jpg', width: 400, height: 400 },
-    { src: '/images/vision.jpg', width: 400, height: 400 },
-    { src: '/images/safety.jpg', width: 400, height: 400 },
-    { src: '/images/customer obsession.jpg', width: 400, height: 400 },
-    { src: '/images/integrity.jpg', width: 400, height: 400 },
-    { src: '/images/accountability.jpg', width: 400, height: 400 },
-  ];
-  
+    { src: '/images/mission.jpg', width: 1429, height: 297 },
+    { src: '/images/vision.jpg', width: 1429, height: 297 },
+    { src: '/images/safety.jpg', width: 1429, height: 297 },
+    { src: '/images/customer obsession.jpg', width: 1429, height: 297 },
+    { src: '/images/integrity.jpg', width: 1429, height: 297 },
+    { src: '/images/accountability.jpg', width: 1429, height: 297 },
+];
