2f06aac9dc04b5f3073d270fcb08c5134e70e7bd
jeffmakuto <hjeffmakuto@gmail.com>
2024-09-20 17:06:38 +0300

    feat: create styles for the policies page for type safety

---
+++ adili/src/types/policies.ts
@@ -0,0 +1,46 @@
+import { ChangeEvent } from 'react'
+
+export type CEOVideoProps = {
+    className?: string;
+}
+
+export type SearchBarProps = {
+    searchTerm: string;
+    onSearchChange: (e: ChangeEvent<HTMLInputElement>) => void;
+}
+
+export type PaginationControlsProps = {
+    currentPage: number;
+    totalPages: number;
+    onPrevious: () => void;
+    onNext: () => void;
+}
+
+export type Policy = {
+    id: string;  
+    title: string;  
+    icon: string;  
+    description: string;  
+    embedSrc: string;
+  
+}
+
+export type PolicyAccordionProps = {
+    policies: Policy[];
+    expandedIndex: number | null;
+    onToggle: (index: number) => void;
+}  
+  
+export type PolicyItemProps = {
+    policy: {
+        id: string;
+      title: string;
+      icon: string;
+      description: string;
+      embedSrc: string;
+    };
+    isExpanded: boolean;
+    index: number;
+    onToggle: (index: number) => void;
+}
+  
