90642f85f86332e30cab659d5b7345a24181cf6f
jeffmakuto <hjeffmakuto@gmail.com>
2024-09-20 18:05:27 +0300

    feat: create env vars for the policies embed links

--- adili/src/data/policies.ts
+++ adili/src/data/policies.ts
@@ -3,14 +3,14 @@ export const policies = [
     id: '1',
     title: 'Integrity Code (IC)',
     description: 'Sign up to the',
-    embedSrc: 'https://kenyaairwaysplc.sharepoint.com/sites/kqdocshares/omnidocs/_layouts/15/embed.aspx?UniqueId=8d6c08a7-cad4-4186-b4b3-a260a89397e8',
+    embedSrc: process.env.NEXT_PUBLIC_IC_EMBED_SRC || '#',
     icon: '/images/integrity-code.JPG',
   },
   {
     id: '2',
     title: 'Partner Code of Conduct (PCC)',
     description: 'Standards and expectations for maintaining transparency, fairness, and ethical practices with our business partners.',
-    embedSrc: 'https://kenyaairwaysplc.sharepoint.com/sites/kqdocshares/omnidocs/_layouts/15/embed.aspx?UniqueId=8d6c08a7-cad4-4186-b4b3-a260a89397e8',
+    embedSrc: process.env.NEXT_PUBLIC_PCC_EMBED_SRC || '#',
     icon: '/images/business-partner.jpg',
   },
 ];
