9b1bb439e3c6a262a9ddfde58c8842f3b1ce8924
jeffmakuto <hjeffmakuto@gmail.com>
2024-09-20 17:03:43 +0300

    feat: create data files that serve the policies page, i.e., policies available, ethics and integrity principles

---
+++ adili/src/data/policies.ts
@@ -0,0 +1,16 @@
+export const policies = [
+  {
+    id: '1',
+    title: 'Integrity Code (IC)',
+    description: 'Sign up to the',
+    embedSrc: 'https://kenyaairwaysplc.sharepoint.com/sites/kqdocshares/omnidocs/_layouts/15/embed.aspx?UniqueId=8d6c08a7-cad4-4186-b4b3-a260a89397e8',
+    icon: '/images/integrity-code.JPG',
+  },
+  {
+    id: '2',
+    title: 'Partner Code of Conduct (PCC)',
+    description: 'Standards and expectations for maintaining transparency, fairness, and ethical practices with our business partners.',
+    embedSrc: 'https://kenyaairwaysplc.sharepoint.com/sites/kqdocshares/omnidocs/_layouts/15/embed.aspx?UniqueId=8d6c08a7-cad4-4186-b4b3-a260a89397e8',
+    icon: '/images/business-partner.jpg',
+  },
+];
