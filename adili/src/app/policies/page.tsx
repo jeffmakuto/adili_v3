e147de0cf9c743bf37c6da7945c0da6b52b546ed
jeffmakuto <hjeffmakuto@gmail.com>
2024-09-20 17:02:26 +0300

    feat: update the policies page with message from CEO, Ethics Principles, and Policies logic

--- adili/src/app/policies/page.tsx
+++ adili/src/app/policies/page.tsx
@@ -1,8 +1,23 @@
+import MessageFromCEO from '@/ui/policies/message-from-ceo';
+import EthicsPrinciples from '@/ui/policies/ethics-principles';
+import PoliciesView from '@/ui/policies/policies-view';
 
-export default function Policies() {
+const Policies: React.FC = () => {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1>Ethics & Integrity</h1>
+    <main className="flex flex-col min-h-screen px-4 md:px-8 mt-8">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
+        <div className="shadow-lg">
+          <MessageFromCEO />
+        </div>
+        <div className="shadow-lg">
+          <EthicsPrinciples />
+        </div>
+      </div>
+      <div className="flex-1 mt-16 p-4">
+        <PoliciesView />
+      </div>
     </main>
   );
 }
+
+export default Policies;
