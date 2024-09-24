bad41f2280a66c6f5ff2f7be90cd2ae635796569
jeffmakuto <hjeffmakuto@gmail.com>
2024-09-24 13:45:43 +0300

    style: adjust sizes of the icons and make them responsive

--- adili/src/ui/conflict-of-interest/commitment.tsx
+++ adili/src/ui/conflict-of-interest/commitment.tsx
@@ -3,9 +3,12 @@ import { heading, paragraph } from '@/styles/values';
 
 const Commitment: React.FC = () => {
     return (
-        <div className="bg-[#F9E9E4] border-l-4 border-red-600 p-4 mb-8 rounded shadow-md">
+        <div className="bg-[#F9E9E4] border-l-4 border-red-600 p-4 mb-8 rounded-lg shadow-lg">
             <div className="flex items-center">
-                <FaExclamationTriangle className="mr-2 mt-4 align-middle" size={24} aria-label="Our Commitment Icon" />
+                <FaExclamationTriangle
+                    className="mr-2 mt-4 w-4 h-4 md:w-6 md:h-6 lg:w-8 lg:h-8"
+                    aria-label="Our Commitment Icon"
+                    />
                 <h2 className={heading}>Our Commitment</h2>
             </div>
             <p className={paragraph}>
--- adili/src/ui/conflict-of-interest/understanding-conflict-of-interest.tsx
+++ adili/src/ui/conflict-of-interest/understanding-conflict-of-interest.tsx
@@ -3,16 +3,19 @@ import { heading, paragraph } from '@/styles/values'
 
 const UnderstandingConflictOfInterest: React.FC = () => {
     return (
-        <div className="bg-[#F9E9E4] border-l-4 border-red-600 p-4 mb-6 rounded shadow-md">
+        <div className="bg-[#F9E9E4] border-l-4 border-red-600 p-4 mb-6 rounded-lg shadow-lg">
             <div className="flex items-center">
-                <FaClipboardCheck className="text-red-600 mr-2 mt-4" size={24} aria-label="Understanding Conflict of Interest Icon" />
+                <FaClipboardCheck
+                    className="mr-2 mt-4 w-4 h-4 md:w-6 md:h-6 lg:w-8 lg:h-8"
+                    aria-label="Understanding Conflict of Interest Icon"
+                />
                 <h2 className={heading}>Understanding Conflict of Interest</h2>
             </div>
             <p className={paragraph}>
-                For an employee, conflict of interest arises when your <strong>personal activities and relationships interfere or appear</strong>
-                to interfere with your ability to perform your duty in the best interest of the Company. Board members and staff shall be expected
+                For an employee, conflict of interest arises when your <strong>personal activities and relationships interfere or appear</strong> to
+                interfere with your ability to perform your duty in the best interest of the Company. Board members and staff shall be expected
                 not to use the power and authority vested in their roles in violation of their professional responsibility or for personal
-                benefit. ​
+                benefit.
             </p>
         </div>
     );
