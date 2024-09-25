ad3a593b4c1138b5a080df5e318e64119bc8aefb
jeffmakuto <hjeffmakuto@gmail.com>
2024-09-25 09:56:14 +0300

    feat: create company assets page components

---
+++ adili/src/ui/company-assets/accuracy.tsx
@@ -0,0 +1,22 @@
+import { FaCheckCircle } from 'react-icons/fa'
+import { heading, paragraph } from '@/styles/values'
+
+const MaintainingInformationAccuracy: React.FC = () => {
+    return (
+        <div className="bg-[#F9E9E4] border-l-4 border-red-600 p-4 mb-6 rounded-lg shadow-lg">
+            <div className="flex items-center">
+                <FaCheckCircle
+                    className="mr-2 mt-4 w-4 h-4 md:w-6 md:h-6 lg:w-8 lg:h-8"
+                    aria-label="Understanding Conflict of Interest Icon"
+                />
+                <h2 className={heading}>Maintaining Information Accuracy</h2>
+            </div>
+            <p className={paragraph}>
+                We must ensure information is kept accuracy, including contracts, and pricing 
+                information to marketing plans, technical specifications and employee information.
+            </p>
+        </div>
+    );
+};
+
+export default MaintainingInformationAccuracy;
---
+++ adili/src/ui/company-assets/compliance.tsx
@@ -0,0 +1,22 @@
+import { FaFileContract } from 'react-icons/fa'
+import { heading, paragraph } from '@/styles/values'
+
+const ComplianceWithPolicies: React.FC = () => {
+    return (
+        <div className="bg-[#F9E9E4] border-l-4 border-red-600 p-4 mb-6 rounded-lg shadow-lg">
+            <div className="flex items-center">
+                <FaFileContract
+                    className="mr-2 mt-4 w-4 h-4 md:w-6 md:h-6 lg:w-8 lg:h-8"
+                    aria-label="Understanding Conflict of Interest Icon"
+                />
+                <h2 className={heading}>Compliance with Policies</h2>
+            </div>
+            <p className={paragraph}>
+                We must all comply to the KQ information security policies and data protection 
+                policies to guide you on how to handle information.
+            </p>
+        </div>
+    );
+};
+
+export default ComplianceWithPolicies;
