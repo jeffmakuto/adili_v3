674e96af24d0bfd083d95bd9825c06ce0bb909f1
jeffmakuto <hjeffmakuto@gmail.com>
2024-09-25 09:52:48 +0300

    feat: create external activities page.tsx and import required files

---
+++ adili/src/app/policies/external-activities/page.tsx
@@ -0,0 +1,47 @@
+'use client'
+
+import HeroImage from '@/ui/conflict-of-interest/hero-image'
+import Header from '@/ui/external-activities/header'
+import CommunicationGuidelines from '@/ui/external-activities/guidelines'
+import RespectingEmployeeRights from '@/ui/external-activities/rights'
+
+const EthicalMarketing: React.FC = () => {
+    const renderHeroImage = (src: string, alt: string) => (
+        <HeroImage src={src} alt={alt} width={800} height={600} />
+    );
+
+    return (
+        <main>
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6 p-4 md:p-4 mt-8">
+            {/* First Section */}
+            <section className="flex flex-col">
+                <div className="flex justify-center py-2 mb-4 shadow-lg rounded-lg">
+                    {renderHeroImage("/images/external-activities.jpg", "Ethical Marketing Crew Image")}
+                </div>
+                <div className="flex flex-col justify-center p-4">
+                    <div>
+                        <Header />
+                    </div>
+                    <div className="mt-16 lg:mt-20 hover:scale-105">
+                        <CommunicationGuidelines />
+                    </div>
+                </div>
+            </section>
+
+            {/* Second Section */}
+            <section className="flex flex-col">
+                <div className="flex justify-center py-2 mb-4 md:mt-16 lg:mt-28 shadow-lg rounded-lg">
+                    {renderHeroImage("/images/external-activities.webp", "Ethical Marketing Anime")}
+                </div>
+                <div className="flex flex-col justify-center mt-12 md:mt-20 lg:mt-32 p-4 hover:scale-105">
+                    <div>
+                        <RespectingEmployeeRights />
+                    </div>
+                </div>
+            </section>
+            </div>
+        </main>
+    );
+};
+
+export default EthicalMarketing;
