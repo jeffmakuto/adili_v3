31076fdde553a211e66060832f03765d8d8f6f22
jeffmakuto <hjeffmakuto@gmail.com>
2024-09-25 11:08:50 +0300

    fix: use a better fa icon

--- adili/src/ui/preventing-crimes/dilligence.tsx
+++ adili/src/ui/preventing-crimes/dilligence.tsx
@@ -1,11 +1,11 @@
-import { FaFileInvoice } from 'react-icons/fa'
+import { FaSearch } from 'react-icons/fa'
 import { heading, paragraph } from '@/styles/values'
 
 const DueDiligenceResponsibilities: React.FC = () => {
     return (
         <div className="p-4 mb-6">
             <div className="flex items-center">
-                <FaFileInvoice
+                <FaSearch
                     className="mr-2 mt-4 w-4 h-4 md:w-6 md:h-6 lg:w-8 lg:h-8"
                     aria-label="Due Diligence Responsibilities Icon"
                 />
--- adili/src/ui/preventing-crimes/illegal.tsx
+++ adili/src/ui/preventing-crimes/illegal.tsx
@@ -1,11 +1,11 @@
-import { FaFileInvoice } from 'react-icons/fa'
+import { FaRegFrown } from 'react-icons/fa'
 import { heading, paragraph } from '@/styles/values'
 
 const NoSupportForIllegalActivities: React.FC = () => {
     return (
         <div className="bg-[#F9E9E4] border-l-4 border-red-600 p-4 mb-6 rounded-lg shadow-lg">
             <div className="flex items-center">
-                <FaFileInvoice
+                <FaRegFrown
                     className="mr-2 mt-4 w-4 h-4 md:w-6 md:h-6 lg:w-8 lg:h-8"
                     aria-label="No Support for Illegal Activities Icon"
                 />
