61dd529042634733c8965029dab4b01852d9aee9
jeffmakuto <hjeffmakuto@gmail.com>
2024-10-02 10:59:49 +0300

    feat: make the Sexual Harrassment Policy a link

--- adili/src/ui/safeguarding/commitment.tsx
+++ adili/src/ui/safeguarding/commitment.tsx
@@ -2,20 +2,23 @@ import { FaPeopleCarry } from 'react-icons/fa'
 import { heading, paragraph } from '@/styles/values'
 
 const Commitment: React.FC = () => {
+    const sexualHarassmentPolicyUrl = process.env.NEXT_PUBLIC_SEXUAL_HARASSMENT_POLICY_URL || '#';
+
     return (
         <div className="bg-[#F9E9E4] border-l-4 border-red-600 p-4 mb-8 rounded-lg shadow-lg">
             <div className="flex items-center">
                 <FaPeopleCarry
                     className="mr-2 mt-4 w-4 h-4 md:w-6 md:h-6 lg:w-8 lg:h-8"
                     aria-label="Our Commitment Icon"
-                    />
+                />
                 <h2 className={heading}>Our Commitment</h2>
             </div>
             <p className={paragraph}>
                 At Kenya Airways, we are committed to maintaining a workplace that is safe, 
                 inclusive and respectful. For this reason, we have a zero tolerance to 
-                any form of sexual harassment.  The <strong>Sexual Harassment Policy</strong> provides more 
-                details on this.​
+                any form of sexual harassment. 
+                The <a href={sexualHarassmentPolicyUrl} target="_blank" rel="noopener noreferrer" className="text-red-600 underline">Sexual 
+                    Harassment Policy</a> provides more details on this.
             </p>
         </div>
     );
