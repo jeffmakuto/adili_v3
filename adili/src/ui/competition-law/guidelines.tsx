b973ed9511f9121d7f1649c24a83bcbbd2de13f1
jeffmakuto <hjeffmakuto@gmail.com>
2024-10-02 11:04:09 +0300

    feat: create link to the Integrity Code

--- adili/src/ui/competition-law/guidelines.tsx
+++ adili/src/ui/competition-law/guidelines.tsx
@@ -2,6 +2,8 @@ import { FaRegHandshake } from 'react-icons/fa'
 import { heading, paragraph } from '@/styles/values'
 
 const Guidelines: React.FC = () => {
+    const integrityCodeUrl = process.env.NEXT_PUBLIC_IC_EMBED_SRC || '#';
+
     return (
         <div className="bg-[#F9E9E4] border-l-4 border-red-600 p-4 mb-6 rounded-lg shadow-lg">
             <div className="flex items-center">
@@ -12,7 +14,8 @@ const Guidelines: React.FC = () => {
                 <h2 className={heading}>Engagement Guidelines</h2>
             </div>
             <p className={paragraph}>
-                <strong>Competitor and customer engagement guidelines</strong> are provided in the Integrity Code to 
+                <strong>Competitor and customer engagement guidelines</strong> are provided in 
+                the <a href={integrityCodeUrl} target="_blank" rel="noopener noreferrer" className="text-red-600 underline">Integrity Code</a> to 
                 ensure all interactions are above board.
             </p>
         </div>
