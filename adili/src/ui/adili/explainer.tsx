eb146661a52e92f5b1f231d3e14f50a62a7d81c6
jeffmakuto <hjeffmakuto@gmail.com>
2024-10-14 09:47:41 +0300

    refactor: rename the title

--- adili/src/ui/adili/explainer.tsx
+++ adili/src/ui/adili/explainer.tsx
@@ -5,7 +5,7 @@ const Explainer: React.FC = () => {
   return (
     <div className="text-center pt-8 md:pt-16 lg:pt-8">
       <h2 className={heading}>
-        How Adili Chat Works
+        Meet Adili
       </h2>
       <div className="flex justify-center items-center p-4">
         <div className="w-full max-w-4xl p-4"> 
