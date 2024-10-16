683ea292942196002e95859a72b9c348ab764afa
jeffmakuto <hjeffmakuto@gmail.com>
2024-10-16 13:05:18 +0300

    style; restyle the adili page

--- adili/src/ui/adili/explainer.tsx
+++ adili/src/ui/adili/explainer.tsx
@@ -1,12 +1,11 @@
-import { heading } from '@/styles/values'
 import ExplainerVideo from '@/ui/adili/explainer-video'
 
 const Explainer: React.FC = () => {
   return (
     <div className="text-center pt-8 md:pt-16 lg:pt-8">
-      <h2 className={heading}>
+      <h1 className="text-xl md:text-2xl lg:text-4xl text-red-600 font-black mb-6">
         Meet Adili
-      </h2>
+      </h1>
       <div className="flex justify-center items-center p-4">
         <div className="w-full max-w-4xl p-4"> 
           <ExplainerVideo className="w-full" />
