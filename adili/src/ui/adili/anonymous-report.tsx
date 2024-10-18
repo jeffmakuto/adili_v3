eab2687dfafeb0bbaaab58412be60812a4788613
jeffmakuto <hjeffmakuto@gmail.com>
2024-10-18 14:44:55 +0300

    style: improve styling of the title and text

--- adili/src/ui/adili/anonymous-report.tsx
+++ adili/src/ui/adili/anonymous-report.tsx
@@ -1,4 +1,5 @@
-import QRCode from "../codes/qr-code";
+import { heading, paragraph } from '@/styles/values'
+import QRCode from '@/ui/codes/qr-code'
 
 const ReportAnonymously: React.FC = () => {
   const reportUrl = process.env.NEXT_PUBLIC_KQ_ETHICS_HOTLINE_URL || '#';
@@ -6,16 +7,16 @@ const ReportAnonymously: React.FC = () => {
   return (
     <div className="flex items-center bg-white shadow-lg rounded-lg p-4 border border-gray-200 mt-4">
       <div className="flex-1">
-        <h2 className="text-xl font-semibold mb-3">Report Anonymously</h2>
+        <h2 className={heading}>Report Anonymously</h2>
         <a
           href={reportUrl}
           target="_blank"
           rel="noopener noreferrer"
-          className="text-red-600 hover:underline"
+          className={`text-red-600 hover:underline ${paragraph}`}
         >
           Click here to report an issue
         </a>
-        <span className="text-gray-700 ml-2">or scan the QR Code</span>
+        <span className={paragraph}> or scan the QR Code</span>
       </div>
       <div className="ml-4 md:hidden">
         <QRCode width={100} height={100} />
--- adili/src/ui/adili/contact-us.tsx
+++ adili/src/ui/adili/contact-us.tsx
@@ -1,10 +1,12 @@
+import { heading, paragraph } from '@/styles/values'
+
 const ContactUs: React.FC = () => {
   const contactEmail = process.env.NEXT_PUBLIC_CONTACT_EMAIL || '#';
 
   return (
     <div className="bg-white shadow-lg rounded-lg p-4 border border-gray-200 mt-4" style={{ flex: 1 }}>
-      <h2 className="text-xl font-semibold mb-3">Contact Us</h2>
-      <p>
+      <h2 className={heading}>Contact Us</h2>
+      <p className={paragraph}>
         If you have any questions, feel free to{' '}
         <a href={`mailto:${contactEmail}`} className="text-red-600 hover:underline">
           email us
