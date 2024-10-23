953308882e07bc7e925dd2a409fbbceab1b97ee8
jeffmakuto <hjeffmakuto@gmail.com>
2024-10-23 14:56:21 +0300

    feat; prevent horizontal scroll effect

--- adili/src/ui/general/motion-slider.tsx
+++ adili/src/ui/general/motion-slider.tsx
@@ -1,17 +1,20 @@
-import { motion } from 'framer-motion'
-import { MotionSliderProps } from '@/types/slider'
+import { motion } from 'framer-motion';
+import { MotionSliderProps } from '@/types/slider';
 
 const MotionSlider: React.FC<MotionSliderProps> = ({ currentIndex, direction, children }) => {
   return (
-    <motion.div
-      initial={{ x: direction === 'right' ? 100 : -100, opacity: 0 }}
-      animate={{ x: 0, opacity: 1 }}
-      exit={{ x: direction === 'right' ? -100 : 100, opacity: 0 }}
-      transition={{ duration: 0.5, ease: 'easeInOut' }}
-      key={currentIndex}
-    >
-      {children}
-    </motion.div>
+    <div style={{ overflow: 'hidden', width: '100%' }}>
+      <motion.div
+        initial={{ x: direction === 'right' ? '100%' : '-100%', opacity: 0 }}
+        animate={{ x: 0, opacity: 1 }}
+        exit={{ x: direction === 'right' ? '-100%' : '100%', opacity: 0 }}
+        transition={{ duration: 0.5, ease: 'easeInOut' }}
+        key={currentIndex}
+        style={{ width: '100%' }}
+      >
+        {children}
+      </motion.div>
+    </div>
   );
 };
 
