cf5baa4960f5a7020d5d3f05f20a08d4bb51e382
jeffmakuto <hjeffmakuto@gmail.com>
2024-11-22 13:12:39 +0300

    feat: add bulletin

--- adili/src/data/bulletins.ts
+++ adili/src/data/bulletins.ts
@@ -3,4 +3,5 @@ import { ImageData } from '@/types/slider'
 export const images: ImageData[] = [
     { src: '/images/chopdekar.png', width: 1128, height: 1278 },
     { src: '/images/KQstars.png', width: 1127, height: 1277 },
+    { src: '/images/enoch.png', width: 2254, height: 2554 },
 ];
